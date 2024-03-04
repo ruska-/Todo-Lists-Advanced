af3c750311c0e53a186ea64ec6eb304e5d1ef9e3
ruska- <ruska.beridze@gmail.com>
2024-03-04 15:27:00 +0400

    use local storage hook from previous projects

---
+++ src/useLocalStorage.js
@@ -0,0 +1,24 @@
+import { useEffect, useState } from "react";
+
+export function useLocalStorage(key, initialValue) {
+  const [value, setValue] = useState(() => {
+    let result = initialValue;
+    const storedValue = localStorage.getItem(key);
+
+    if (typeof initialValue === "function") {
+      result = initialValue();
+    }
+
+    if (storedValue !== null) {
+      result = JSON.parse(storedValue);
+    }
+
+    return result;
+  });
+
+  useEffect(() => {
+    localStorage.setItem(key, JSON.stringify(value));
+  }, [value, key]);
+
+  return [value, setValue];
+}
